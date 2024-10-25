4d75904ad74004314aeb6a3b50c074264086a166
Younes <70694280+YMblgh@users.noreply.github.com>
2024-10-25 23:26:51 +0330

    todo component updated.

--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { MyBtnComponent } from "../my-btn/my-btn.component";
+import { FormsModule } from '@angular/forms';
 
 interface TodoItemInterface {
   title: string,
@@ -9,22 +10,25 @@ interface TodoItemInterface {
 @Component({
   selector: 'app-todo-list',
   standalone: true,
-  imports: [MyBtnComponent],
+  imports: [MyBtnComponent, FormsModule],
   templateUrl: './todo-list.component.html',
   styleUrl: './todo-list.component.scss'
 })
 export class TodoListComponent {
   protected todoList: TodoItemInterface[] = []
   todoInput: string = ''
-  @Input() ngModel = ''
 
-  addTodoHandler(e: MouseEvent) {
-    this.todoList?.push({ title: 'hi', check: false })
+  addTodoHandler() {
+    if (this.todoInput.length != 0) {
+      this.todoList?.push({ title: this.todoInput, check: false })
+      this.todoInput = ''
+      document.getElementById('#todo-input')?.classList.toggle('border-red-300', false)
+    } else {
+      document.getElementById('#todo-input')?.classList.toggle('border-red-300', true)
+    }
     console.log(this.todoList);
-    
   }
   inputChangeHandler(e: Event) {
-    console.log(e);
-    
+    console.log(this.todoInput);
   }
 }
