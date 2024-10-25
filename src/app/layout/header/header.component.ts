79afc5747ec16912a6a53e3f2ebb48e92651b40b
Younes <70694280+YMblgh@users.noreply.github.com>
2024-10-25 12:45:52 +0330

    header updated.

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatDrawer, MatDrawerContainer, MatDrawerContent } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule, 
+    MatDrawerContainer,
+    MatDrawer,
+    MatDrawerContent,
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
